9ec30b79c00b10bdb6e500a54c1c92e4491dc151
Richard Montgomery <rikermonty@gmail.com>
2021-02-25 16:41:50 -0800

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import Card from './Card';
+
+function Home() {
+    return (
+        <main>
+            <Card />
+            <Card />
+            <Card />
+        </main>
+    )
+}
+
+export default Home;
